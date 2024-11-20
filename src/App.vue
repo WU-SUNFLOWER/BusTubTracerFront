bd8aef241b5d047e8b0b6f0d568c4af0ec4873fc
余准 <108219118+MODEMINGZ@users.noreply.github.com>
2024-11-20 09:18:51 +0800

    路由

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,188 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <svg id="mainsvg" width="500" height="1000"></svg>
+  <!-- <div class="container">
+    <div class="left"></div>
+    <div class="middle">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
     </div>
-  </header>
+    <div class="right"></div>
 
-  <RouterView />
+  </div> -->
 </template>
 
+<script setup lang="ts">
+import axios from "axios"
+import { ref, onMounted } from 'vue';
+import * as d3 from 'd3';
+import dagreD3 from 'dagre-d3';
+
+var process_data: any;
+
+function getProcessInfo() {
+  axios.get('api/process_info.json').then(res => {
+    process_data = res.data;
+    console.log(process_data);
+  }).catch(err => {
+    console.log(err);
+  });
+}
+
+getProcessInfo();
+
+const planner_tree = {
+  "planner_node_tag": "Projection",
+  "planner_node_id": 0,
+  "attr": {
+    "exprs": ["#0.0", "#0.1", "#0.2", "#0.3"]
+  },
+  "children": [{
+    "planner_node_tag": "Filter",
+    "planner_node_id": 1,
+    "attr": {
+      "predicate": ["(#0.0=#0.2)"]
+    },
+    "children": [{
+      "planner_node_tag": "NestedLoopJoin",
+      "planner_node_id": 2,
+      "attr": {
+        "type": "Inner",
+        "predicate": true
+      },
+      "children": [{
+        "planner_node_id": 3,
+        "planner_node_tag": "SeqScan",
+        "attr": {
+          "table": "students"
+        }
+      },
+      {
+        "planner_node_id": 4,
+        "planner_node_tag": "SeqScan",
+        "attr": {
+          "table": "courses_selected_record"
+        }
+      },
+      ]
+    }]
+  }]
+};
+
+
+function treeToGraphData(tree: any) {
+  const data = { nodes: [] as any[], edges: [] as any[], plannerNodeId: -1 };
+  const nodeIdMap = new Map();
+  let maxPlannerNodeId = -1;
+
+  function traverseTree(node: { planner_node_id: any; planner_node_tag: any; children: any; }, parentId = null) {
+    const nodeId = node.planner_node_id;
+    const nodeIndex = data.nodes.length;
+    data.nodes.push({ id: nodeId, label: node.planner_node_tag, shape: 'ellipse' });
+    nodeIdMap.set(nodeId, nodeIndex);
+    if (parentId !== null) {
+      const parentIndex = nodeIdMap.get(parentId);
+      data.edges.push({ source: nodeIndex, target: parentIndex, label: '' });
+    }
+    maxPlannerNodeId = Math.max(maxPlannerNodeId, nodeId);
+    (node.children || []).forEach((child: { planner_node_id: any; planner_node_tag: any; children: any; }) => traverseTree(child, nodeId));
+  }
+
+  function traverseTable(node: { attr: { table: any; }; planner_node_id: any; children: any; }) {
+    if (node.attr && node.attr.table) {
+      maxPlannerNodeId = maxPlannerNodeId + 1;
+      const newNodeId = maxPlannerNodeId;
+      data.nodes.push({ id: newNodeId, label: node.attr.table, shape: 'rect' });
+      data.edges.push({ source: newNodeId, target: node.planner_node_id, label: '' });
+    }
+
+    (node.children || []).forEach((child: { attr: { table: any; }; planner_node_id: any; children: any; }) => traverseTable(child));
+  }
+
+  traverseTree(tree);
+  data.plannerNodeId = maxPlannerNodeId;
+  traverseTable(tree);
+
+  return data;
+};
+
+const data = treeToGraphData(planner_tree);
+let container: any;
+let g: any;
+let nowNode = -1;
+
+function getNodeStyle(item: any, isPlannerNode: boolean) {
+  let baseStyle = `fill:${isPlannerNode ? 'rgb(147,210,243)' : 'rgb(162,239,77)'};stroke:none;cursor:pointer`;
+  let labelStyle = `fill:rgb(0,0,0);font-weight:bold;cursor:pointer`;
+  if (nowNode == item.id && isPlannerNode) {
+    baseStyle = `fill:rgb(147,210,243);stroke:purple;stroke-width:3px;stroke-dasharray:5,5;cursor:pointer`;
+  }
+  return {
+    style: baseStyle,
+    labelStyle: labelStyle,
+  };
+}
+
+function drawTree() {
+  // 清除之前的渲染
+  d3.select('#mainsvg').selectAll('*').remove();
+  var graph: any;
+  graph = new dagreD3.graphlib.Graph();
+  graph.setGraph({ rankdir: 'BT' });
+
+  data.nodes.forEach((item: any) => {
+    const isPlannerNode = item.id <= data.plannerNodeId;
+    const { style, labelStyle } = getNodeStyle(item, isPlannerNode);
+    graph.setNode(item.id, {
+      label: item.label,
+      shape: item.shape,
+      style,
+      labelStyle,
+    });
+  });
+
+  data.edges.forEach((item: any) => {
+    graph.setEdge(item.source, item.target, {
+      label: item.label,
+      style: "fill:#fff;stroke:rgb(249,74,86);stroke-width:1.5px",
+      arrowheadStyle: "fill: rgb(249,74,86);",
+    });
+  });
+
+  const render = new dagreD3.render();
+  container = d3.select('#mainsvg').append('g');
+  render(container, graph);
+  g = graph;
+
+  container.selectAll('g.node').on('click', (e: any) => {
+    nowNode = e.target.__data__;
+    drawTree();
+  });
+}
+
+onMounted(() => {
+  drawTree();
+});
+
+
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.container {
+  display: flex;
+  justify-content: space-between;
+  height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.left {
+  width: 20%;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.middle {
+  width: 50%;
+  border-left: 1px solid black;
+  border-right: 1px solid black;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.right {
+  width: 30%;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
-      component: HomeView,
+      component: () => import("../views/Home.vue"),
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/index',
+      name: 'index',
+      component: () => import("../views/Index.vue"),
     },
+    {
+      path: '/process',
+      name: 'process',
+      component: () => import("../views/Process.vue"),
+    },
+    {
+      path: '/sqllogs',
+      name: 'sqllogs',
+      component: () => import("../views/SQLLogs.vue"),
+    },
+    {
+      path: '/storage',
+      name: 'storage',
+      component: () => import("../views/Storage.vue"),
+    },
+    {
+      path: '/help',
+      name: 'help',
+      component: () => import("../views/Help.vue"),
+    },
+    {
+      path: '/:catchAll(.*)',
+      redirect: '/home'
+    }
   ],
 })
 

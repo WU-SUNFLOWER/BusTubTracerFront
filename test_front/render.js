67302b4b2448d480c8a39dc42102b74ee64e8bf2
WU-SUNFLOWER <47438910+WU-SUNFLOWER@users.noreply.github.com>
2024-11-23 02:31:38 +0800

    test_front页面加入对返回错误消息的显示

--- test_front/render.js
+++ test_front/render.js
@@ -1,13 +1,17 @@
 const button = document.getElementById("submit_button");
 const response_elem = document.getElementById("response");
 const text_input = document.getElementById("text_input");
+const err_elem = document.getElementById("err_msg");
 
 button.onclick = async () => {
     let data = {
         "sql": text_input.value,
     };
-    console.log("before response");
     let response = await window.bustub.sendMessage('/submit_sql_command', data);
-    console.log("after response");
-    response_elem.innerText = response.data.raw_result;
+    if (response.err_msg) {
+        err_elem.innerText = response.err_msg;
+    } else {
+        response_elem.innerText = response.data.raw_result;
+    }
+    
 };
